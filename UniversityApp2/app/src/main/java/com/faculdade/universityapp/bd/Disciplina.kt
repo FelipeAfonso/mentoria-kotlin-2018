396335e9f87f08c7c048aca3d9016cbc7eb3761c
Vinicius José Ribeiro de Oliveira <viniciusjose.19@gmail.com>
2018-08-22 16:30:10 -0300

    UniversityApp2
    Versão 2 - App Kotlin + SQLite
    Obs: Base de dados funcionando corretamente (Insert e Select testados)

---
+++ UniversityApp2/app/src/main/java/com/faculdade/universityapp/bd/Disciplina.kt
@@ -0,0 +1,9 @@
+package com.faculdade.universityapp.bd
+
+class Disciplina (val id : Int,
+                  val sigla : String,
+                  val nome : String,
+                  val semAno : Int,
+                  val nota : Double)
+
+
