e680d680bd6e730f760d2644270e80f9a64386f1
Vinicius José Ribeiro de Oliveira <viniciusjose.19@gmail.com>
2018-08-22 16:51:46 -0300

    UniversityApp2.1
    Versão 2.1 - APK Kotlin + SQLite
    News: Base de dados configurada corretamente (Funções Insert e Select funcionando)

--- UniversityApp2/app/src/main/java/com/faculdade/universityapp/bd/Disciplina.kt
+++
@@ -1,9 +0,0 @@
-package com.faculdade.universityapp.bd
-
-class Disciplina (val id : Int,
-                  val sigla : String,
-                  val nome : String,
-                  val semAno : Int,
-                  val nota : Double)
-
-
